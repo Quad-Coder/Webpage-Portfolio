5f3e4e2dee152ee088c546f71ef87120cfb7c168
Quadrick <quadtreeder@gmail.com>
2021-10-04 12:38:57 -0700

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Navbar(){
+    return (
+        <>
+            <nav className="navbar">
+                <div className="nav-container">
+                    <Link to="/" className="navbar-logo">
+                        
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
