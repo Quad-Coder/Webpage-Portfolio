25fa2e5847af237b333245afef3638a22a626cdc
Quadrick <quadtreeder@gmail.com>
2022-02-16 17:10:17 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
 import "./App.css";
-import Header from "../src/Header/Header";
 
 function App() {
-  return <Header />;
+  return (
+    <body>
+      <header>
+        <div class="home-profile-img center-block text-center">
+          <img src="home_portrait.jpg" alt="Quadrick"></img>
+        </div>
+        <div class="col-md-8">
+          <h1 class="title-text clickable">Quadrick Reeder</h1>
+        </div>
+      </header>
+      <main></main>
+    </body>
+  );
 }
 
 export default App;
