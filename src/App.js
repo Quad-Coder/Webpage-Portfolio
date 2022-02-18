fd2a0607bb26ba83480577e26939533aeac98b4e
Quadrick <quadtreeder@gmail.com>
2022-02-18 11:18:59 -0800

    Aligned Profile Pic W/ Name & Positions

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ function App() {
   return (
     <body>
       <header>
-        <div class="home-profile-img center-block text-center">
-          <img src="home_portrait.jpg" alt="Quadrick"></img>
-        </div>
-        <div class="col-md-8">
-          <h1>Quadrick Reeder</h1>
-          <h2>Full-Stack Software Developer</h2>
+        <div class="Header-container">
+          <div class="home-profile-img">
+            <img src="home_portrait.jpg" alt="Quadrick"></img>
+          </div>
+          <div class="col-md-8">
+            <h1>Quadrick Reeder</h1>
+            <h2>Full-Stack Software Developer</h2>
+          </div>
         </div>
       </header>
       <main></main>
