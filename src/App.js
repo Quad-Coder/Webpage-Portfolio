ef5ccae08e3a1237ccffbc97a124e6852f37ce57
Quadrick <quadtreeder@gmail.com>
2022-02-19 12:32:40 -0800

    Added buttons, links, and fixed spacing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,26 @@ function App() {
           <div class="col-md-8">
             <h1>Quadrick Reeder</h1>
             <h2>Full-Stack Software Developer</h2>
+            <div class="profile-header-btns text-center">
+              <a
+                href="https://www.linkedin.com/in/quadrick-reeder-538437207/"
+                class="hidden-print btn btn-ar btn-info button-glow button-rounded button-raised btn-lg"
+              >
+                <i class="fa fa-linkedin"></i>" LinkedIn"
+              </a>
+              <a
+                href="https://github.com/Quad-Coder"
+                class="hidden-print btn btn-ar btn-info button-glow button-rounded button-raised btn-lg"
+              >
+                <i class="fa fa-github"></i>" GitHub"
+              </a>
+              <a
+                href="#contact"
+                class="hidden-print btn btn-ar btn-danger button-glow button-rounded button-raised btn-lg"
+              >
+                <i class="fa fa-envelope-o"></i>" Contact"
+              </a>
+            </div>
           </div>
         </div>
       </header>
