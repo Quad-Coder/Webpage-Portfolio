65ff5f9a6aff002105a1e661cae6bbe62f77fdc2
Quadrick <quadtreeder@gmail.com>
2022-02-21 09:24:55 -0800

    Added basic Who Am I title and text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./App.html";
 
 function App() {
   return (
@@ -16,25 +17,41 @@ function App() {
                 href="https://www.linkedin.com/in/quadrick-reeder-538437207/"
                 class="hidden-print btn btn-ar btn-info button-glow button-rounded button-raised btn-lg"
               >
-                <i class="fa fa-linkedin"></i>" LinkedIn"
+                <i class="fa fa-linkedin"></i> LinkedIn
               </a>
               <a
                 href="https://github.com/Quad-Coder"
                 class="hidden-print btn btn-ar btn-primary button-glow button-rounded button-raised btn-lg"
               >
-                <i class="fa fa-github"></i>" GitHub"
-              </a>
-              <a
-                href="#contact"
-                class="hidden-print btn btn-ar btn-danger button-glow button-rounded button-raised btn-lg"
-              >
-                <i class="fa fa-envelope-o"></i>" Contact"
+                <i class="fa fa-github"></i> GitHub
               </a>
             </div>
           </div>
         </div>
       </header>
-      <main></main>
+      <main>
+        <section id="about" class>
+          <div class="info-card accent-border">
+            <div class="info-body flex">
+              <div class="info-title">
+                <h1>Who Am I</h1>
+              </div>
+              <hr></hr>
+              <div class="info-text">
+                <p>
+              I am a Full Stack Developer with a background in customer service
+              and warehouse production. I like to learn new skills and
+              information and don’t like to be stagnant. In previous years of
+              customer service, I learned this about myself. I want to break
+              into the tech industry because I believe technology is only going
+              to become more dominant in the future. Interested in opportunities
+              to apply my training and experience to support appropriate teams.
+            </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </body>
   );
 }
